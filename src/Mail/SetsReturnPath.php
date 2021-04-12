18351e747e71d8e5c4a413cf4645d561f1b8376a
Elisha Witte <elisha.witte@antwerpes.de>
2021-04-12 20:31:13 +0200

    Add return path trait for mailables

---
+++ src/Mail/SetsReturnPath.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Chiiya\Common\Mail;
+
+/**
+ * Trait SetsReturnPath.
+ *
+ * @property array $callbacks
+ */
+trait SetsReturnPath
+{
+    /**
+     * Set the return path for the message.
+     *
+     * @return $this
+     */
+    public function returnPath(string $returnPath): self
+    {
+        $this->callbacks[] = function ($message) use ($returnPath) {
+            $message->returnPath($returnPath);
+        };
+
+        return $this;
+    }
+}
